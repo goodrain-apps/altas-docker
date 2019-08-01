1c700c034558885fefc64e5e5210432c3cfc6b21
guox <guox@goodrain.com>
2019-08-01 16:44:59 +0800

    config with ENVs

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /usr/local/mysql-proxy
 ADD https://github.com/Qihoo360/Atlas/releases/download/2.2.1/Atlas-2.2.1.el6.x86_64.rpm ./
 RUN rpm -i Atlas-2.2.1.el6.x86_64.rpm && rm -rf Atlas-2.2.1.el6.x86_64.rpm
 ADD docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+ENV ALTAS_HOME /usr/local/mysql-proxy
 
 ENTRYPOINT [ "/usr/local/bin/docker-entrypoint.sh" ]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 [[ $DEBUG ]] && set -x
 
+#加密mysql密码
+MYSQL_ENCRYPT_PASS=$(${ALTAS_HOME}/bin/encrypt ${MYSQL_PASSWORD})
+MYSQL_ROOT_ENCRYPT_PASS=$(${ALTAS_HOME}/bin/encrypt ${MYSQL_ROOT_PASSWORD})
 
+sed -i -e "s/root:rootpass, mysqluser:mysqlpassword/root:${MYSQL_ROOT_ENCRYPT_PASS}, ${MYSQL_USER}:${MYSQL_ENCRYPT_PASS}/g" \
+       -e "s/proxy-address = 0.0.0.0:3308/proxy-address = 0.0.0.0:${PORT:-3308}/g" \
+       -e "s/admin-username = user/admin-username = ${ADMIN_USER:-user}/g" \
+       -e "s/admin-password = pwd/admin-password = ${ADMIN_PASS:-pwd}/g" \
+       -e "s/admin-address = 0.0.0.0:2345/admin-address = 0.0.0.0:${ADMIN_PORT:-2345}/g" ${ALTAS_HOME}/conf/test.cnf
+
+[[ $CHARSET ]] && sed -i "s/#charset = utf8/charset = ${CHARSET}/g" ${ALTAS_HOME}/conf/test.cnf
 
 [[ $PAUSE ]] && sleep $PAUSE
+
+/usr/local/mysql-proxy/bin/mysql-proxyd test start

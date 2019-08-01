03680d74b132baa0c689e09dbfef096e351ea608
guox <guox@goodrain.com>
2019-08-01 23:55:52 +0800

    for now, init slave mysql in atlas

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM centos:6
 WORKDIR /usr/local/mysql-proxy
 ADD https://github.com/Qihoo360/Atlas/releases/download/2.2.1/Atlas-2.2.1.el6.x86_64.rpm ./
-RUN rpm -i Atlas-2.2.1.el6.x86_64.rpm && rm -rf Atlas-2.2.1.el6.x86_64.rpm
+RUN rpm -i Atlas-2.2.1.el6.x86_64.rpm && rm -rf Atlas-2.2.1.el6.x86_64.rpm && \
+    yum makecache fast && \
+    yum install mysql -y && \
+    yum clean all && \
+    rm -rf /var/cache/yum
 ADD docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
 ADD test.cnf /usr/local/mysql-proxy/conf/test.cnf
 ENV ALTAS_HOME /usr/local/mysql-proxy
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,4 +16,20 @@ sed -i -e "s/root:rootpass, mysqluser:mysqlpassword/root:${MYSQL_ROOT_ENCRYPT_PA
 
 [[ $PAUSE ]] && sleep $PAUSE
 
+# mk mysql slave
+while true 
+do
+# start slave 
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQL_SLAVE_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password='${MYSQL_ROOT_PASSWORD}';start slave;"
+# check it
+mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQL_SLAVE_PORT} -e "show slave status\G;" | grep "Slave_IO_Running: Yes"
+if [ $? == 0 ];then    
+    break
+fi
+echo "Waiting slave DB init··· retry in 3 seconds" 
+sleep 3
+done
+
+
+
 /usr/local/mysql-proxy/bin/mysql-proxyd test start

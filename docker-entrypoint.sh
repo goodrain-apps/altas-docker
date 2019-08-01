a1a185502d79493aa9c03edbe843fd0f91bb67b0
guox <guox@goodrain.com>
2019-08-02 01:01:50 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@
 #加密mysql密码
 MYSQL_ENCRYPT_PASS=$(${ALTAS_HOME}/bin/encrypt ${MYSQL_PASSWORD})
 MYSQL_ROOT_ENCRYPT_PASS=$(${ALTAS_HOME}/bin/encrypt ${MYSQL_ROOT_PASSWORD})
-
+[[ $PAUSE ]] && sleep $PAUSE
 sed -i -r -e "s%root:rootpass, mysqluser:mysqlpassword%root:${MYSQL_ROOT_ENCRYPT_PASS}, ${MYSQL_USER}:${MYSQL_ENCRYPT_PASS}%g" \
-       -e "s/proxy-address = 0.0.0.0:3308/proxy-address = 0.0.0.0:${PORT:-3308}/g" \
+       -e "s/proxy-address = 0.0.0.0:2345/proxy-address = 0.0.0.0:${PORT:-3308}/g" \
        -e "s/admin-username = user/admin-username = ${ADMIN_USER:-user}/g" \
        -e "s/admin-password = pwd/admin-password = ${ADMIN_PASS:-pwd}/g" \
        -e "s/admin-address = 0.0.0.0:2345/admin-address = 0.0.0.0:${ADMIN_PORT:-2345}/g" ${ALTAS_HOME}/conf/test.cnf

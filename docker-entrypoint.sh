dd25752f53e7af9ad1a209bcd9457e676980943a
guox <guox@goodrain.com>
2019-08-02 09:10:57 +0800

    Update docker-entrypoint.sh
    debug mysql slave init

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -26,7 +26,7 @@ mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQL_SLAVE_PORT} -e "show slave statu
 if [ $? == 0 ];then    
     break
 else 
-    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQL_SLAVE_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password='${MYSQL_ROOT_PASSWORD}';start slave;"
+    mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQL_SLAVE_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password='${MYSQL_ROOT_PASSWORD}';DROP USER '${MYSQL_USER}'@'%';start slave;"
 fi
 echo "Waiting slave DB init··· retry in 3 seconds" 
 

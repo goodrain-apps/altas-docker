1936769cff1b71d18ba6dac7fd6e9180505e7273
guox <guox@goodrain.com>
2019-08-22 09:07:34 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,23 +16,4 @@ sed -i -r -e "s%root:rootpass, mysqluser:mysqlpassword%root:${MYSQL_ROOT_ENCRYPT
 
 [[ $PAUSE ]] && sleep $PAUSE
 
-# # mk mysql slave
-# while true 
-# do
-# # start slave 
-# sleep 10
-# # check it
-# mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQL_SLAVE_PORT} -e "show slave status\G;" | grep "Slave_IO_Running: Yes"
-# if [ $? == 0 ];then    
-#     break
-# else 
-#     mysql -uroot -p${MYSQL_ROOT_PASSWORD} -P${MYSQL_SLAVE_PORT} -e "change master to master_host='127.0.0.1', master_port=3306, master_user='root', master_password='${MYSQL_ROOT_PASSWORD}';DROP USER '${MYSQL_USER}'@'%';start slave;"
-# fi
-# echo "Waiting slave DB init··· retry in 3 seconds" 
-
-# done
-
-
-
-
 /usr/local/mysql-proxy/bin/mysql-proxyd test start
